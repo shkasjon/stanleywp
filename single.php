6cf07fce5076353fb1adf65bc1ccbfededae5be8
Luca Spiller <luca@stackednotion.com>
2014-07-14 22:22:08 +0100

    Add mailchimp

--- single.php
+++ single.php
@@ -83,6 +83,37 @@
                             <div class="post-edit"><?php edit_post_link( __( 'Edit', 'gents' ) ); ?></div>
                           </footer>
 
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+<div id="mc_embed_signup">
+<form action="//blissfulsystems.us4.list-manage.com/subscribe/post?u=6d99e1bc4bf9e096fae044198&amp;id=cd9c8b33a6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+  <h2>Get the latest jobs straight to your inbox</h2>
+<p>Our weekly newsletter contains the latest jobs before they are available anywhere else.</p>
+<div class="mc-field-group">
+  <label for="mce-FNAME">Your name </label>
+  <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+</div>
+<div class="mc-field-group">
+  <label for="mce-EMAIL">Your email </label>
+  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+  <div id="mce-responses" class="clear">
+    <div class="response" id="mce-error-response" style="display:none"></div>
+    <div class="response" id="mce-success-response" style="display:none"></div>
+  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_6d99e1bc4bf9e096fae044198_cd9c8b33a6" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+<script type='text/javascript'>
+(function($) {
+  window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[0]='EMAIL';ftypes[0]='email';
+}(jQuery));
+var $mcj = jQuery.noConflict(true);
+</script>
+<!--End mc_embed_signup-->
+
 
                         </div>
                       </div>
